b67cafced7495c841c7f0d561b14754b3c0cd768
squigley <squigley@shutterstock.com>
2014-11-10 02:50:53 -0500

    untar and build kafka

--- puppet/modules/kafka/manifests/init.pp
+++ puppet/modules/kafka/manifests/init.pp
@@ -4,9 +4,30 @@ class kafka (
 
   exec { 'download-kafka-package':
     cwd     => '/home/vagrant',
-    command => '/usr/bin/wget https://archive.apache.org/dist/kafka/kafka-0.8.0-beta1-src.tgz',
-    onlyif  => '/usr/bin/test ! -e /home/vagrant/kafka-0.8.0-beta1-src.tgz',
+    command => 'wget https://archive.apache.org/dist/kafka/kafka-0.8.0-beta1-src.tgz',
+    creates => '/home/vagrant/kafka-0.8.0-beta1-src.tgz',
+    path    => ['/usr/bin', '/bin'],
   }
 
+  file { '/usr/local/kafka':
+    ensure => 'directory',
+  }
+
+  exec { 'untar-kafka':
+    cwd     => '/home/vagrant',
+    command => 'tar -zxvf kafka-0.8.0-beta1-src.tgz -C /usr/local/kafka',
+    path    => ['/usr/bin', '/bin'],
+    creates => '/usr/local/kafka/kafka-0.8.0-beta1-src',
+    require => [ Exec['download-kafka-package'], File['/usr/local/kafka'] ],
+  }
+
+  exec { 'sbt-build':
+    cwd      => '/usr/local/kafka/kafka-0.8.0-beta1-src',
+    command  => './sbt update && ./sbt package && ./sbt assembly-package-dependency',
+    provider => shell,
+    path     => ['/usr/local/kafka/kafka-0.8.0-beta1-src', '/usr/bin'],
+    creates  => '/usr/local/kafka/kafka-0.8.0-beta1-src/target',
+    require  => Exec['untar-kafka'],
+  }
 
 }
