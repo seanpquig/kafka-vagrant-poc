e9b06163041604ac7a6cb461b65c862a9f802471
squigley <squigley@shutterstock.com>
2014-11-11 23:17:46 -0500

    added template for zoo.cfg file

--- puppet/modules/zookeeper/manifests/init.pp
+++ puppet/modules/zookeeper/manifests/init.pp
@@ -15,12 +15,19 @@ class zookeeper () {
     cwd     => '/home/vagrant',
     command => 'tar -zxvf zookeeper-3.4.6.tar.gz -C /usr/local/zookeeper',
     path    => ['/usr/bin', '/bin'],
-    creates => '/usr/local/zookeeper/zookeeper-3.4.6.tar.gz',
+    creates => '/usr/local/zookeeper/zookeeper-3.4.6',
     require => [ Exec['download-zookeeper'], File['/usr/local/zookeeper'] ],
   }
 
   file { [ '/var/zookeeper', '/var/zookeeper/data' ]:
     ensure => 'directory',
+  }
+
+  file { 'zoo.cfg':
+    ensure  => present,
+    path    => '/usr/local/zookeeper/zookeeper-3.4.6/conf/zoo.cfg',
+    content => template('zookeeper/zoo.cfg'),
+    require => Exec['untar-zookeeper'],
   }
 
 }
