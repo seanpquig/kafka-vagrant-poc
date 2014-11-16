72887d82d51e427194418715136baa4d77cc714e
squigley <squigley@shutterstock.com>
2014-11-16 18:10:15 -0500

    added zookeeper host to hiera and reference in kafka server.properties

--- puppet/modules/kafka/manifests/init.pp
+++ puppet/modules/kafka/manifests/init.pp
@@ -31,6 +31,8 @@ class kafka (
     require  => Exec['untar-kafka'],
   }
 
+  $zookeeper_host = hiera('zookeeper_host')
+
   file { 'server.properties':
     ensure  => present,
     path    => '/usr/local/kafka/kafka-0.8.0-beta1-src/config/server.properties',
