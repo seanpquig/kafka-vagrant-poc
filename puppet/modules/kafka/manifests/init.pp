4b0e1997911c2fe83211fffdc9a6b1c09e1ef76e
squigley <squigley@shutterstock.com>
2014-11-17 00:50:09 -0500

    pull kafka config into its own manifest and add pgrep checks to process start commands

--- puppet/modules/kafka/manifests/init.pp
+++ puppet/modules/kafka/manifests/init.pp
@@ -2,41 +2,15 @@ class kafka (
   $broker_id  = 1,
 ) {
 
-  exec { 'download-kafka-package':
-    cwd     => '/home/vagrant',
-    command => 'wget https://archive.apache.org/dist/kafka/kafka-0.8.0-beta1-src.tgz',
-    creates => '/home/vagrant/kafka-0.8.0-beta1-src.tgz',
+  class { 'kafka::config': 
+    broker_id => $broker_id,
+  }
+
+  exec { 'start-kafka':
+    command => '/usr/local/kafka/kafka-0.8.0-beta1-src/bin/kafka-server-start.sh /usr/local/kafka/kafka-0.8.0-beta1-src/config/server.properties &',
     path    => ['/usr/bin', '/bin'],
-  }
-
-  file { '/usr/local/kafka':
-    ensure => 'directory',
-  }
-
-  exec { 'untar-kafka':
-    cwd     => '/home/vagrant',
-    command => 'tar -zxvf kafka-0.8.0-beta1-src.tgz -C /usr/local/kafka',
-    path    => ['/usr/bin', '/bin'],
-    creates => '/usr/local/kafka/kafka-0.8.0-beta1-src',
-    require => [ Exec['download-kafka-package'], File['/usr/local/kafka'] ],
-  }
-
-  exec { 'sbt-build':
-    cwd      => '/usr/local/kafka/kafka-0.8.0-beta1-src',
-    command  => './sbt update && ./sbt package && ./sbt assembly-package-dependency',
-    provider => shell,
-    timeout  => 600,
-    path     => ['/usr/local/kafka/kafka-0.8.0-beta1-src', '/usr/bin'],
-    creates  => '/usr/local/kafka/kafka-0.8.0-beta1-src/target',
-    require  => Exec['untar-kafka'],
-  }
-
-  $zookeeper_host = hiera('zookeeper_host')
-
-  file { 'server.properties':
-    ensure  => present,
-    path    => '/usr/local/kafka/kafka-0.8.0-beta1-src/config/server.properties',
-    content => template('kafka/server.properties.erb'),
+    unless  => 'pgrep -f kafka',
+    require => Class['kafka::config'],
   }
 
 }
--- puppet/modules/zookeeper/manifests/init.pp
+++ puppet/modules/zookeeper/manifests/init.pp
@@ -4,6 +4,8 @@ class zookeeper () {
 
   exec { 'start-zookeeper':
     command => '/usr/local/zookeeper/zookeeper-3.4.6/bin/zkServer.sh start',
+    path    => ['/usr/bin', '/bin'],
+    unless  => 'pgrep -f zookeeper',
     require => Class['zookeeper::config'],
   }
 
